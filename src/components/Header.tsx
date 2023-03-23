05dd72b5bf1618cf24f0b3d2a076825c3f234d8d
ajay <ajayjarwal44@gmail.com>
2023-03-23 16:31:25 +0530

    implement headr component

---
+++ src/components/Header.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import styles from "../styles/Header.module.css";
+
+export default function Header() {
+  return (
+    <div className={styles.header}>
+      <div className={styles.header__info}>
+        <p>
+          Hi, my name is Ajay. <span>I am a web developer</span>
+          from Dausa, Rajasthan.
+        </p>
+      </div>
+      <div className={styles.header__divider}></div>
+      <div className={styles.header__pic}>
+        <Image
+          src={"/profile.png"}
+          width={120}
+          height={120}
+          alt="profile pic"
+        />
+      </div>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,9 +14,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className={styles.main}>
-        <h2>Hello world</h2>
+        <Header />
       </main>
     </>
   );
