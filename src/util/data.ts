4a08ad36c50002ee4bf98b90ce87b9d6ea2c5ca5
ajay <ajayjarwal44@gmail.com>
2023-03-25 18:01:08 +0530

    add project link to data file

--- src/util/data.ts
+++ src/util/data.ts
@@ -3,6 +3,7 @@ export const projectData = [
     id: "1",
     title: "Rhyme",
     imgSrc: "poem.png",
+    projectLink: "https://rhyme-ajay020.vercel.app/",
     description: `
         Rhyme is an app for sharing poems. An authenticated user can write, update, and delete a poem. 
         For backend, I used Expressjs and Mongodb, and for frontend, NextJS and HTML, CSS.
@@ -12,6 +13,8 @@ export const projectData = [
     id: "2",
     title: "Expense Tracker",
     imgSrc: "expense_tracker.png",
+    projectLink:
+      "https://62c3fd5335671334f23fedf9--expense-tracker-ajay020.netlify.app/",
     description: `
         In this app, we can calculate daily expenses.
         For frontend, I used Reactjs, HTML, and CSS.
@@ -21,6 +24,7 @@ export const projectData = [
     id: "3",
     title: "MoodTracker",
     imgSrc: "moodtracker.png",
+    projectLink: "https://moodtracker-ajay.netlify.app/",
     description: `
     In this app, we can log our daily mood, and we can analyze our emotions.  
     I have built this app with Reactjs, HTML, and CSS.
@@ -30,6 +34,7 @@ export const projectData = [
     id: "4",
     title: "Todo",
     imgSrc: "todo.png",
+    projectLink: "https://ajay020.github.io/todo-app/",
     description: `
     In this app, we can add a task, update and delete a task. 
     I have built this with Reactjs, HTML, and CSS.
@@ -39,6 +44,7 @@ export const projectData = [
     id: "5",
     title: "Calculator",
     imgSrc: "calculator.png",
+    projectLink: "https://ajay020.github.io/calculator-app/",
     description: `
     This is a simple calculator.
     I used Reactjs, HTML, and CSS.
