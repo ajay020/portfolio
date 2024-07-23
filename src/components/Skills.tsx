6817ea064e432d02d204aa3c79321cc84708d9e8
ajay <ajayjarwal44@gmail.com>
2024-07-23 15:49:50 +0530

    update skill section layout and add android in it

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -5,6 +5,7 @@ import {
   faReact,
   faHtml5,
   faJs,
+  faAndroid,
   faGit,
   faNodeJs,
 } from "@fortawesome/free-brands-svg-icons";
@@ -13,29 +14,29 @@ import styles from "../styles/Skills.module.css";
 
 const Skills: React.FC = () => {
   return (
-    <section id="skills" className={styles.skills}>
+    <section id="skills" className={`${styles.skills} my-4`}>
       <h2 className="text-3xl font-bold">Skills</h2>
-      <div className="flex  justify-center flex-col md:flex-row gap-4  ">
-        <div className={`border rounded w-[400px]`}>
-          <h3 className="text-center my-3">Frontend</h3>
-          <ul className={`${styles.skill_list} `}>
-            <SkillItem name="JavaScript" icon={faJs} color="#f2d518" />
-            <SkillItem name="HTML" icon={faHtml5} color="#f21f18" />
-            <SkillItem name="CSS" icon={faCss3} color="#eef218" />
-            <SkillItem name="React" icon={faReact} color="#3cbff4" />
-            {/* <SkillItem name="React Native" icon={faReact} color="#3cbff4" /> */}
-            <SkillItem name="Redux" icon={faReact} color="#3cbff4" />
-          </ul>
+      <div className={`
+        my-12 flex gap-4 p-12 border
+          flex justify-center w-1/2 
+          m-auto`}>
+        <div>
+          <SkillItem name="Android" icon={faAndroid} color="#0ac620" />
+          <SkillItem name="React" icon={faReact} color="#3cbff4" />
+          <SkillItem name="React Native" icon={faReact} color="#3cbff4" />
         </div>
-        <div className={`border rounded w-[400px]`}>
-          <h3 className="my-3 text-center">Backend</h3>
-          <ul className={`${styles.skill_list} w-[400px]`}>
-            <SkillItem name="JavaScript" icon={faJs} color="#f2d518" />
-            <SkillItem name="Node.js" icon={faNodeJs} color="#3cf451" />
-            <SkillItem name="Express" icon={faExpand} color="#ab0606" />
-            <SkillItem name="MongoDB" icon={faDatabase} color="#0ac620" />
-          </ul>
+        <div>
+          <SkillItem name="JavaScript" icon={faJs} color="#f2d518" />
+          <SkillItem name="HTML" icon={faHtml5} color="#f21f18" />
+          <SkillItem name="CSS" icon={faCss3} color="#eef218" />
         </div>
+
+        <div>
+          <SkillItem name="Node.js" icon={faNodeJs} color="#3cf451" />
+          <SkillItem name="Express" icon={faExpand} color="#ab0606" />
+          <SkillItem name="MongoDB" icon={faDatabase} color="#0ac620" />
+        </div>
+
       </div>
     </section>
   );
@@ -49,10 +50,10 @@ interface SkillItemProps {
 
 const SkillItem: React.FC<SkillItemProps> = ({ name, icon, color }) => {
   return (
-    <li className={styles.skill_list_item}>
+    <div className={styles.skill_list_item}>
       <span className="text-sm pr-2">{name}</span>
       <FontAwesomeIcon icon={icon} size="lg" style={{ color }} />
-    </li>
+    </div>
   );
 };
 
